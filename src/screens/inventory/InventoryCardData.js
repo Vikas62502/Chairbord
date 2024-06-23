8f5855ce609aede15d41962186306b4fb2d92350
vikas62502 <vikashyadav62502@gmail.com>
2024-06-23 19:19:36 +0530

    inventiry section completed

--- src/screens/inventory/InventoryCardData.js
+++ src/screens/inventory/InventoryCardData.js
@@ -1,44 +1,19 @@
+
+
+
 const inventoryCardData = [
     {
-        logo: require("../../assets/screens/wallet/walletCardData/tagCostRefund.png"),
-        title: "Tag cost refund",
-        description: "Received purchase refund...",
-        amount: "855",
-        date: "16-03-2024",
-        time: "01:03:25",
-        ID: "096245465",
-        RefNo: "096245465"
+        color: "#5C50A4",
+        number: 4,
     },
-    // {
-    //     logo: require("../../assets/screens/wallet/walletCardData/mobilerecharge.png"),
-    //     title: "Mobile Recharge",
-    //     description: "Received purchase refund...",
-    //     amount: "-595",
-    //     date: "16-03-2024",
-    //     time: "01:03:25",
-    //     ID: "096245465",
-    //     RefNo: "096245465"
-    // },
-    // {
-    //     logo: require("../../assets/screens/wallet/walletCardData/Shopping.png"),
-    //     title: "Shopping",
-    //     description: "Received purchase refund...",
-    //     amount: "-2055",
-    //     date: "16-03-2024",
-    //     time: "01:03:25",
-    //     ID: "096245465",
-    //     RefNo: "096245465"
-    // },
-    // {
-    //     logo: require("../../assets/screens/wallet/walletCardData/medicine.png"),
-    //     title: "Medicine",
-    //     description: "Received purchase refund...",
-    //     amount: "-655",
-    //     date: "16-03-2024",
-    //     time: "01:03:25",
-    //     ID: "096245465",
-    //     RefNo: "096245465"
-    // },
+    {
+        color: "#EB9C02",
+        number: 5,
+    },
+    {
+        color: "#02EB19",
+        number: 6,
+    }
 ]
 
 export default inventoryCardData;
