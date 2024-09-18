4ee4d164fc329c2fe0fdff98b168744857e6aa4e
vikas62502 <vikashyadav62502@gmail.com>
2024-09-18 14:45:06 +0530

    Update Axios.ts

--- src/client/Axios.ts
+++ src/client/Axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import { setupInterceptorsTo } from './interceptor'
 
-const customBaseUrl = 'http://192.168.0.65'
+const customBaseUrl = 'http://43.204.133.228'
 
 export const client = axios.create({
   baseURL: `${customBaseUrl}:3001/v1/api`,
